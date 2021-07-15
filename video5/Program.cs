ae9097d77d8f8dfa1c5aa10737ac8211f8cad97d
Akram-Boktor <35446384+AkramBoktor@users.noreply.github.com>
2021-07-15 06:04:53 +0200

    Add files via upload

---
+++ video5/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace video5
+{
+    public delegate Car CarFactoryDel(int id, string name);
+    public delegate void logDetailsIceCarContravariance(ICEcar icecar);
+    public delegate void logDetailsEvCarContravariance(EVcar eVcar);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarFactoryDel carFactoryDel = CarFactory.ReturnCar;
+            Car icecar = carFactoryDel(1, "BMW");
+
+            Console.WriteLine($"{icecar.GetType()}");
+            Console.WriteLine($"{icecar.GetDetails()}");
+
+            Console.WriteLine("-------------------------------------------------");
+            carFactoryDel = CarFactory.ReturnEvCar;
+            Car evcar = carFactoryDel(1, "Tesla");
+            Console.WriteLine($"{evcar.GetType()}");
+            Console.WriteLine($"{evcar.GetDetails()}");
+
+            Console.WriteLine("-----------------------Contravariance--------------------------");
+            logDetailsIceCarContravariance logDetailsIceCarContravariance = logCarDetails;
+            logDetailsIceCarContravariance(icecar as ICEcar);
+
+            logDetailsEvCarContravariance logDetailsEvCarContravariance = logCarDetails;
+            logDetailsEvCarContravariance(evcar as EVcar);
+
+            Console.ReadKey();
+        }
+
+
+        static void logCarDetails(Car car)
+        {
+            if(car is ICEcar)
+            {
+                Console.WriteLine($"{car.GetType()}");
+                Console.WriteLine($"{car.GetDetails()}");
+            }
+            else if(car is EVcar) 
+            {
+                Console.WriteLine($"{car.GetType()}");
+                Console.WriteLine($"{car.GetDetails()}");
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+
+    public static class CarFactory
+    {
+        public static ICEcar ReturnCar(int id , string name)
+        {
+            return new ICEcar { Id = id, Name = name };
+
+        }
+
+        public static EVcar ReturnEvCar(int id, string name)
+        {
+            return new EVcar { Id = id, Name = name };
+
+        }
+    } 
+    public abstract class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual string GetDetails()
+        {
+           return $"{Id} - {Name}";
+        } 
+    }
+
+    public class ICEcar : Car
+    {
+        public override string GetDetails()
+        {
+            return base.GetDetails();
+        }
+    }
+
+    public class EVcar : Car
+    {
+        public override string GetDetails()
+        {
+            return base.GetDetails();
+        }
+    }
+}
